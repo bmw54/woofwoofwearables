c0be78a225c9d3ea5d7924ae0841fd277fbcc976
Doherty Guirand <57299921+dohertyguirand@users.noreply.github.com>
2022-02-08 18:14:53 -0500

    Update Messaging_Server.py

--- Communication/Socket/Messaging_Server.py
+++ Communication/Socket/Messaging_Server.py
@@ -1,6 +1,6 @@
 import socket
 
-HOST = '172.28.42.55' # Server IP or Hostname
+HOST = '172.28.42.55' # Wireless IP Address of PI
 PORT = 12345 # Pick an open Port (1000+ recommended), must match the client sport
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('Socket created')
