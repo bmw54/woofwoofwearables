ae6b6535358c8f8868cb5fc948aa68ecf655afb2
Doherty Guirand <dg211@duke.edu>
2022-02-10 16:37:18 -0500

    Wrote client to recieve sensor data

---
+++ Communication/Socket/Sensor_Client.py
@@ -0,0 +1,11 @@
+import socket
+
+HOST = '172.28.143.162' # Wireless IP Address of PI
+PORT = 12345 # Pick an open Port (1000+ recommended), must match the server port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST,PORT))
+
+#Lets loop awaiting for your input
+while True:
+	reply = s.recv(1024).decode()
+	print(reply)
