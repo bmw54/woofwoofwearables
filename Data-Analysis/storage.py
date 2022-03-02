9e5c3363abf40a28c5bba43adcbd20d5da940297
Doherty Guirand <dg211@duke.edu>
2022-03-02 18:00:25 -0500

    add data storage script

---
+++ Data-Analysis/storage.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import time
+import board
+import adafruit_icm20x
+import numpy as np
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
+icm = adafruit_icm20x.ICM20948(i2c)
+
+df = pd.DataFrame(columns = ['X_Acceleration','Y_Acceleration', 'Z_Acceleration', 'X_Gyro', 'Y_Gyro','Z_Gyro','X_Magnetometer', 'Y_Magnetometer', 'Z_Magnetometer'])
+
+df = df.append({'Name' : 'Ankit', 'Articles' : 97, 'Improved' : 2200}, 
+                ignore_index = True)
+
+cycles = 1800
+for i in range(0, cycles):
+    acceleration = icm.acceleration
+    gyro = icm.gyro
+    magnetic = icm.magnetic
+
+    print("\n\n\n\nAcceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (icm.acceleration))
+    print("Gyro X:%.2f, Y: %.2f, Z: %.2f rads/s" % (icm.gyro))
+    print("Magnetometer X:%.2f, Y: %.2f, Z: %.2f uT" % (icm.magnetic))
+    print("")
+    df = df.append({'X_Acceleration' : acceleration[0], 'Y_Acceleration' : acceleration[1], 'Z_Acceleration' : acceleration[2], 'X_Gyro' : gyro[0], 'Y_Gyro': gyro[1], 'Z_Gyro' :gyro[2], 'X_Magnetometer': magnetic[0], 'Y_Magnetometer' : magnetic[1], 'Z_Magnetometer' : magnetic[2]}, 
+        ignore_index = True)
+    time.sleep(.1)
+
