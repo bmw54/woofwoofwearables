2c32bd272a4f1c0c6f28248bf2d384dc0da87ec3
Braeden <bmw54@duke.edu>
2022-03-22 20:54:00 -0400

    Began process of interpolating data to correct fro inconsistent sampling frequency

--- BasicIMUScripts/WorkingWithCongoData/CongoDataFiltering.py
+++ BasicIMUScripts/WorkingWithCongoData/CongoDataFiltering.py
@@ -1,6 +1,24 @@
 import json
+import skinematics as skin
+import statistics as stat
+import numpy as np
 
-from numpy import size
+
+def interpolate(xlist, ylist, xval):
+    if xval != np.clip(xval, min(xlist), max(xlist)):
+        print("X value", xval, "out of range")
+        xval = np.clip(xval, min(xlist), max(xlist))
+        print("Rounding X value to:", xval)
+    
+    if xlist.count(xval): return ylist[xlist.index(xval)]
+
+    x1_ind = [i for i in range(len(xlist)) if xlist[i]<xval][-1]
+    x1 = xlist[x1_ind]
+    x2 = xlist[x1_ind+1]
+    y1 = ylist[x1_ind]
+    y2 = ylist[x1_ind+1]
+    yval = ((y2-y1)*1.0/(x2-x1)) * (xval-x1) + y1
+    return yval
 
 with open("woof-woof-wearables-default-rtdb-2-push-export.json", "r") as read_file:
     read_data = json.load(read_file)
@@ -27,11 +45,12 @@ with open("woof-woof-wearables-default-rtdb-2-push-export.json", "r") as read_fi
 
     # Construct some dictionaries
     # because magnetometer is missing values at index 6 and 16, we'll start after those indeces
+    # edit: actually there seems to be a large break in the values for about a little over a minute, so we'll start after that
     # MAKE SURE PYTHON VERSION IS 3.6 OR LATER SO THAT DICTIONARIES MAINTAIN ORDER
     # Note: using the acceleration x value timestamps for everything
 
     data = {}
-    for i in range(17,len(accltx)):
+    for i in range(35,len(accltx)):
         new_accl = {'X': acclx[list(acclx.keys())[i]]["Value"],
                     'Y': accly[list(accly.keys())[i]]["Value"],
                     'Z': acclz[list(acclz.keys())[i]]["Value"]}
@@ -46,6 +65,24 @@ with open("woof-woof-wearables-default-rtdb-2-push-export.json", "r") as read_fi
         
         data[accltx[i]] = {'ACCL': new_accl, 'GYRO': new_gyro, 'MAG': new_mag}
 
-    # print(list(data.values())[0])
-    print(data)
+    times = list(data)
+    timeDiffs = [times[i+1] - times[i] for i in range(len(times)-1)]
+
+    for t in timeDiffs: print(t / stat.median(timeDiffs))
+    print("timespan: ", times[-1] - times[0])
+    print("timestep: ", stat.median(timeDiffs))
+    print("timesteps: ", (times[-1] - times[0])/stat.median(timeDiffs))
+
+    numsteps = round((times[-1] - times[0])/stat.median(timeDiffs))
+    print("numsteps:", numsteps)
+    timestep = (times[-1] - times[0])/numsteps
+    print("timestep:", timestep)
+    corrected_times = np.linspace(times[0], times[-1], numsteps)
+    
+    xlist = [0,1,2]
+    ylist = [0,1,0]
+    xvals = [0, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.25]
+
+    intperpolations = [interpolate(xlist, ylist, xval) for xval in xvals]
+    print("interpolatons:", intps)
 
