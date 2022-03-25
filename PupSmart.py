927132778653d505e33dd569a74f7c45a3db66d3
Doherty Guirand <dg211@duke.edu>
2022-03-25 14:20:48 -0700

    changed pupsmart have to test

--- PupSmart.py
+++ PupSmart.py
@@ -1,3 +1,4 @@
+from sklearn.neighbors import VALID_METRICS
 from Communication.Firebase.RPi2Firebase import RPi2Firebase
 from BasicIMUScripts.IMUDataModule import IMUDataModule
 from Camera.CameraModule import CameraModule
@@ -7,7 +8,30 @@ data_module = IMUDataModule()
 rpi_2_firebase = RPi2Firebase()
 # camera_module = CameraModule()
 
-while True:
+trial_name = input("Trial run Name: ")
+duration = 0.0
+try:
+    duration = int(input("Trial Duration (seconds): "))
+except ValueError:
+    print("Duration must be a number")
+    duration = int(input("Trial Duration (seconds): "))
+
+
+x_gyro_list = []
+y_gyro_list = []
+z_gyro_list = []
+
+x_accel_list = []
+y_accel_list = []
+z_accel_list = []
+
+x_mag_list = []
+y_mag_list = []
+z_mag_list = []
+
+time_start = time.time()
+
+while time.time() < time_start + duration:
     data_module.poll_imu_data()
     timestamp = data_module.get_timestamp()
     x_acceleration = data_module.get_X_acceleration()
@@ -22,35 +46,39 @@ while True:
     y_mag = data_module.get_Y_magnetic()
     z_mag = data_module.get_Z_magnetic()
 
-    x_accel_data = {"Time" : timestamp, "Value": x_acceleration}
-    y_accel_data = {"Time" : timestamp, "Value": y_acceleration}
-    z_accel_data = {"Time" : timestamp, "Value": z_acceleration}
+    x_accel_list.append({"Time" : timestamp, "Value": x_acceleration}) 
+    y_accel_list.append( {"Time" : timestamp, "Value": y_acceleration})
+    z_accel_list.append({"Time" : timestamp, "Value": z_acceleration})
 
-    x_gyro_data = {"Time" : timestamp, "Value": x_gyro}
-    y_gyro_data = {"Time" : timestamp, "Value": y_gyro}
-    z_gyro_data = {"Time" : timestamp, "Value": z_gyro}
+    x_gyro_list.append({"Time" : timestamp, "Value": x_gyro})
+    y_gyro_list.append({"Time" : timestamp, "Value": y_gyro})
+    z_gyro_list.append({"Time" : timestamp, "Value": z_gyro})
 
-    x_mag_data = {"Time" : timestamp, "Value": x_mag}
-    y_mag_data = {"Time" : timestamp, "Value": y_mag}
-    z_mag_data = {"Time" : timestamp, "Value": z_mag}
+    x_mag_list.append({"Time" : timestamp, "Value": x_mag})
+    y_mag_list.append({"Time" : timestamp, "Value": y_mag})
+    z_mag_list.append({"Time" : timestamp, "Value": z_mag})
+    time.sleep(.05)
 
-    rpi_2_firebase.send_timeseries_to_firebase(x_accel_data, "March24Run", "X", "accel")
-    rpi_2_firebase.send_timeseries_to_firebase(y_accel_data, "March24Run", "Y", "accel")
-    rpi_2_firebase.send_timeseries_to_firebase(z_accel_data, "March24Run", "Z", "accel")
+print(trial_name + "trial complete after" + time.time() - time_start + "seconds")
 
-    rpi_2_firebase.send_timeseries_to_firebase(x_gyro_data, "March24Run", "X", "gyro")
-    rpi_2_firebase.send_timeseries_to_firebase(y_gyro_data, "March24Run", "Y", "gyro")
-    rpi_2_firebase.send_timeseries_to_firebase(z_gyro_data, "March24Run", "Z", "gyro")
+for i in range(0, len(x_accel_list)):
+    rpi_2_firebase.send_timeseries_to_firebase(x_accel_list[i], trial_name, "X", "accel")
+    rpi_2_firebase.send_timeseries_to_firebase(y_accel_list[i], trial_name, "Y", "accel")
+    rpi_2_firebase.send_timeseries_to_firebase(z_accel_list[i], trial_name, "Z", "accel")
 
-    rpi_2_firebase.send_timeseries_to_firebase(x_mag_data, "March24Run", "X", "mag")
-    rpi_2_firebase.send_timeseries_to_firebase(y_mag_data, "March24Run", "Y", "mag")
-    rpi_2_firebase.send_timeseries_to_firebase(z_mag_data, "March24Run", "Z", "mag")
+    rpi_2_firebase.send_timeseries_to_firebase(x_gyro_list[i], trial_name, "X", "gyro")
+    rpi_2_firebase.send_timeseries_to_firebase(y_gyro_list[i], trial_name, "Y", "gyro")
+    rpi_2_firebase.send_timeseries_to_firebase(z_gyro_list[i], trial_name, "Z", "gyro")
 
-
+    rpi_2_firebase.send_timeseries_to_firebase(x_mag_list[i], trial_name, "X", "mag")
+    rpi_2_firebase.send_timeseries_to_firebase(y_mag_list[i], trial_name, "Y", "mag")
+    rpi_2_firebase.send_timeseries_to_firebase(z_mag_list[i], trial_name, "Z", "mag")
     current_data = data_module.get_imu_data()
     rpi_2_firebase.send_data_to_firebase(current_data, "CurrentIMUData")
 #     path =  camera_module.take_picture()
 #     rpi_2_firebase.send_image_to_firebase(path, "testimg.jpg")
     # average_data = data_module.get_average_imu_data()
     # rpi_2_firebase.send_data_to_firebase(average_data, "AverageIMUData")
-    time.sleep(.1)
+
+print("Pushing " +trial_name+ "trial to Firebase complete")
+
