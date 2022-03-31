e25c919ad783868dc91846a2319cb95fa520ab71
Doherty Guirand <dg211@duke.edu>
2022-03-31 16:44:35 -0400

    added print

--- PupSmart.py
+++ PupSmart.py
@@ -31,18 +31,18 @@ def collect_data(data_module, x_gyro_list, y_gyro_list, z_gyro_list, x_accel_lis
     z_mag_list.append({"Time" : timestamp, "Value": z_mag})
     return x_gyro_list, y_gyro_list, z_gyro_list, x_accel_list, y_accel_list, z_accel_list, x_mag_list, y_mag_list, z_mag_list
 
-def send_data_to_firebase(x_gyro_list, y_gyro_list, z_gyro_list, x_accel_list, y_accel_list, z_accel_list, x_mag_list, y_mag_list, z_mag_list, trial_name):
-    rpi_2_firebase.send_timeseries_to_firebase(x_accel_list, trial_name, "X", "accel", "2")
-    rpi_2_firebase.send_timeseries_to_firebase(y_accel_list, trial_name, "Y", "accel", "2")
-    rpi_2_firebase.send_timeseries_to_firebase(z_accel_list, trial_name, "Z", "accel", "2")
+def send_data_to_firebase(x_gyro_list, y_gyro_list, z_gyro_list, x_accel_list, y_accel_list, z_accel_list, x_mag_list, y_mag_list, z_mag_list, trial_name, sensor_name):
+    rpi_2_firebase.send_timeseries_to_firebase(x_accel_list, trial_name + "_" + sensor_name, "X", "accel", "2")
+    rpi_2_firebase.send_timeseries_to_firebase(y_accel_list, trial_name + "_" + sensor_name, "Y", "accel", "2")
+    rpi_2_firebase.send_timeseries_to_firebase(z_accel_list, trial_name + "_" + sensor_name, "Z", "accel", "2")
 
-    rpi_2_firebase.send_timeseries_to_firebase(x_gyro_list, trial_name, "X", "gyro", "2")
-    rpi_2_firebase.send_timeseries_to_firebase(y_gyro_list, trial_name, "Y", "gyro", "2")
-    rpi_2_firebase.send_timeseries_to_firebase(z_gyro_list, trial_name, "Z", "gyro", "2")
+    rpi_2_firebase.send_timeseries_to_firebase(x_gyro_list, trial_name + "_" + sensor_name, "X", "gyro", "2")
+    rpi_2_firebase.send_timeseries_to_firebase(y_gyro_list, trial_name + "_" + sensor_name, "Y", "gyro", "2")
+    rpi_2_firebase.send_timeseries_to_firebase(z_gyro_list, trial_name + "_" + sensor_name, "Z", "gyro", "2")
 
-    rpi_2_firebase.send_timeseries_to_firebase(x_mag_list, trial_name, "X", "mag", "2")
-    rpi_2_firebase.send_timeseries_to_firebase(y_mag_list, trial_name, "Y", "mag", "2")
-    rpi_2_firebase.send_timeseries_to_firebase(z_mag_list, trial_name, "Z", "mag", "2")
+    rpi_2_firebase.send_timeseries_to_firebase(x_mag_list, trial_name + "_" + sensor_name, "X", "mag", "2")
+    rpi_2_firebase.send_timeseries_to_firebase(y_mag_list, trial_name + "_" + sensor_name, "Y", "mag", "2")
+    rpi_2_firebase.send_timeseries_to_firebase(z_mag_list, trial_name + "_" + sensor_name, "Z", "mag", "2")
 
 tail_data_module = IMUDataModule(address = 104)
 body_data_module = IMUDataModule(address = 105)
@@ -90,6 +90,7 @@ time_start = time.time()
 
 while time.time() < time_start + duration:
     time_iter = time.time()
+    print(time_iter - time_start)
     tail_x_gyro_list, tail_y_gyro_list, tail_z_gyro_list, tail_x_accel_list, tail_y_accel_list, tail_z_accel_list, tail_x_mag_list, tail_y_mag_list, tail_z_mag_list = collect_data(tail_data_module, tail_x_gyro_list, tail_y_gyro_list, tail_z_gyro_list, tail_x_accel_list, tail_y_accel_list, tail_z_accel_list, tail_x_mag_list, tail_y_mag_list, tail_z_mag_list)
     body_x_gyro_list, body_y_gyro_list, body_z_gyro_list, body_x_accel_list, body_y_accel_list, body_z_accel_list, body_x_mag_list, body_y_mag_list, body_z_mag_list = collect_data(body_data_module, body_x_gyro_list, body_y_gyro_list, body_z_gyro_list, body_x_accel_list, body_y_accel_list, body_z_accel_list, body_x_mag_list, body_y_mag_list, body_z_mag_list)
 
@@ -100,8 +101,8 @@ while time.time() < time_start + duration:
     time.sleep(sample_time - (current_time - time_iter))
 
 print(trial_name + "trial complete after" + str(time.time() - time_start) + "seconds")
-send_data_to_firebase(tail_x_gyro_list, tail_y_gyro_list, tail_z_gyro_list, tail_x_accel_list, tail_y_accel_list, tail_z_accel_list, tail_x_mag_list, tail_y_mag_list, tail_z_mag_list, trial_name)
-send_data_to_firebase(body_x_gyro_list, body_y_gyro_list, body_z_gyro_list, body_x_accel_list, body_y_accel_list, body_z_accel_list, body_x_mag_list, body_y_mag_list, body_z_mag_list, trial_name)
+send_data_to_firebase(tail_x_gyro_list, tail_y_gyro_list, tail_z_gyro_list, tail_x_accel_list, tail_y_accel_list, tail_z_accel_list, tail_x_mag_list, tail_y_mag_list, tail_z_mag_list, trial_name, "Tail")
+send_data_to_firebase(body_x_gyro_list, body_y_gyro_list, body_z_gyro_list, body_x_accel_list, body_y_accel_list, body_z_accel_list, body_x_mag_list, body_y_mag_list, body_z_mag_list, trial_name, "Body")
 #     path =  camera_module.take_picture()
 #     rpi_2_firebase.send_image_to_firebase(path, "testimg.jpg")
     # average_data = tail_data_module.get_average_imu_data()
