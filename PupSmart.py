e467c9ba6eb52c117cf536118dc615d306be75e0
Doherty Guirand <dg211@duke.edu>
2022-04-07 14:54:09 -0400

    changed pupsmart to poll at closer times

--- PupSmart.py
+++ PupSmart.py
@@ -3,33 +3,61 @@ from BasicIMUScripts.IMUDataModule import IMUDataModule
 from Camera.CameraModule import CameraModule
 import time
 
-def collect_data(data_module, x_gyro_list, y_gyro_list, z_gyro_list, x_accel_list, y_accel_list, z_accel_list, x_mag_list, y_mag_list, z_mag_list):
+def collect_data(tail_data_module, body_data_module):
     tail_data_module.poll_imu_data()
+    body_data_module.poll_imu_data()
+
     timestamp = tail_data_module.get_timestamp()
-    x_acceleration = data_module.get_X_acceleration()
-    y_acceleration = data_module.get_Y_acceleration()
-    z_acceleration = data_module.get_Z_acceleration()
+    x_acceleration = tail_data_module.get_X_acceleration()
+    y_acceleration = tail_data_module.get_Y_acceleration()
+    z_acceleration = tail_data_module.get_Z_acceleration()
 
-    x_gyro = data_module.get_X_gyro()
-    y_gyro = data_module.get_Y_gyro()
-    z_gyro = data_module.get_Z_gyro()
+    x_gyro = tail_data_module.get_X_gyro()
+    y_gyro = tail_data_module.get_Y_gyro()
+    z_gyro = tail_data_module.get_Z_gyro()
 
-    x_mag = data_module.get_X_magnetic()
-    y_mag = data_module.get_Y_magnetic()
-    z_mag = data_module.get_Z_magnetic()
+    x_mag = tail_data_module.get_X_magnetic()
+    y_mag = tail_data_module.get_Y_magnetic()
+    z_mag = tail_data_module.get_Z_magnetic()
 
-    x_accel_list.append({"Time" : timestamp, "Value": x_acceleration}) 
-    y_accel_list.append( {"Time" : timestamp, "Value": y_acceleration})
-    z_accel_list.append({"Time" : timestamp, "Value": z_acceleration})
+    x_acceleration = {"Time" : timestamp, "Value": x_acceleration}
+    y_acceleration =  {"Time" : timestamp, "Value": y_acceleration}
+    z_acceleration = {"Time" : timestamp, "Value": z_acceleration}
 
-    x_gyro_list.append({"Time" : timestamp, "Value": x_gyro})
-    y_gyro_list.append({"Time" : timestamp, "Value": y_gyro})
-    z_gyro_list.append({"Time" : timestamp, "Value": z_gyro})
+    x_gyro = {"Time" : timestamp, "Value": x_gyro}
+    y_gyro = {"Time" : timestamp, "Value": y_gyro}
+    z_gyro = {"Time" : timestamp, "Value": z_gyro}
 
-    x_mag_list.append({"Time" : timestamp, "Value": x_mag})
-    y_mag_list.append({"Time" : timestamp, "Value": y_mag})
-    z_mag_list.append({"Time" : timestamp, "Value": z_mag})
-    return x_gyro_list, y_gyro_list, z_gyro_list, x_accel_list, y_accel_list, z_accel_list, x_mag_list, y_mag_list, z_mag_list
+    x_mag = {"Time" : timestamp, "Value": x_mag}
+    y_mag = {"Time" : timestamp, "Value": y_mag}
+    z_mag =  {"Time" : timestamp, "Value": z_mag}
+
+    body_timestamp = body_data_module.get_timestamp()
+    body_x_acceleration = tail_data_module.get_X_acceleration()
+    body_y_acceleration = tail_data_module.get_Y_acceleration()
+    body_z_acceleration = tail_data_module.get_Z_acceleration()
+
+    body_x_gyro = tail_data_module.get_X_gyro()
+    body_y_gyro = tail_data_module.get_Y_gyro()
+    body_z_gyro = tail_data_module.get_Z_gyro()
+
+    body_x_mag = tail_data_module.get_X_magnetic()
+    body_y_mag = tail_data_module.get_Y_magnetic()
+    body_z_mag = tail_data_module.get_Z_magnetic()
+
+    body_x_acceleration = {"Time" : timestamp, "Value": body_x_acceleration}
+    body_y_acceleration =  {"Time" : timestamp, "Value": body_y_acceleration}
+    body_z_acceleration = {"Time" : timestamp, "Value": body_z_acceleration}
+
+    body_x_gyro = {"Time" : timestamp, "Value": body_x_gyro}
+    body_y_gyro = {"Time" : timestamp, "Value": body_y_gyro}
+    body_z_gyro = {"Time" : timestamp, "Value": body_z_gyro}
+
+    body_x_mag = {"Time" : timestamp, "Value": body_x_mag}
+    body_y_mag = {"Time" : timestamp, "Value": body_y_mag}
+    body_z_mag =  {"Time" : timestamp, "Value": body_z_mag}
+    
+    return  x_acceleration, y_acceleration, z_acceleration, x_gyro, y_gyro, z_gyro, x_mag, y_mag, z_mag, body_x_acceleration, body_y_acceleration, body_z_acceleration, body_x_gyro, body_y_gyro, body_z_gyro, body_x_mag, body_y_mag, body_z_mag
 
 def send_data_to_firebase(x_gyro_list, y_gyro_list, z_gyro_list, x_accel_list, y_accel_list, z_accel_list, x_mag_list, y_mag_list, z_mag_list, trial_name):
     rpi_2_firebase.send_timeseries_to_firebase(x_accel_list, trial_name, "X", "accel", "2")
@@ -90,8 +118,26 @@ time_start = time.time()
 
 while time.time() < time_start + duration:
     time_iter = time.time()
-    tail_x_gyro_list, tail_y_gyro_list, tail_z_gyro_list, tail_x_accel_list, tail_y_accel_list, tail_z_accel_list, tail_x_mag_list, tail_y_mag_list, tail_z_mag_list = collect_data(tail_data_module, tail_x_gyro_list, tail_y_gyro_list, tail_z_gyro_list, tail_x_accel_list, tail_y_accel_list, tail_z_accel_list, tail_x_mag_list, tail_y_mag_list, tail_z_mag_list)
-    body_x_gyro_list, body_y_gyro_list, body_z_gyro_list, body_x_accel_list, body_y_accel_list, body_z_accel_list, body_x_mag_list, body_y_mag_list, body_z_mag_list = collect_data(body_data_module, body_x_gyro_list, body_y_gyro_list, body_z_gyro_list, body_x_accel_list, body_y_accel_list, body_z_accel_list, body_x_mag_list, body_y_mag_list, body_z_mag_list)
+    x_acceleration, y_acceleration, z_acceleration, x_gyro, y_gyro, z_gyro, x_mag, y_mag, z_mag, body_x_acceleration, body_y_acceleration, body_z_acceleration, body_x_gyro, body_y_gyro, body_z_gyro, body_x_mag, body_y_mag, body_z_mag = collect_data(tail_data_module, body_data_module)
+    tail_x_accel_list.append(x_acceleration)
+    tail_y_accel_list.append(y_acceleration)
+    tail_z_accel_list.append(z_acceleration)
+    tail_x_gyro_list.append(x_gyro)
+    tail_y_gyro_list.append(y_gyro)
+    tail_z_gyro_list.append(z_gyro)
+    tail_x_mag_list.append(x_mag)
+    tail_y_mag_list.append(y_mag)
+    tail_z_mag_list.append(z_mag)
+
+    body_x_accel_list.append(body_x_acceleration)
+    body_y_accel_list.append(body_y_acceleration)
+    body_z_accel_list.append(body_z_acceleration)
+    body_x_gyro_list.append(body_x_gyro)
+    body_y_gyro_list.append(body_y_gyro)
+    body_z_gyro_list.append(body_z_gyro)
+    body_x_mag_list.append(body_x_mag)
+    body_y_mag_list.append(body_y_mag)
+    body_z_mag_list.append(body_z_mag)
 
     current_time = time.time()
     time_left = sample_time - (current_time - time_iter) #
